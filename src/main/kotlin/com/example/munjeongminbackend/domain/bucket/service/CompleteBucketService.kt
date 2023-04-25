c0afe932f65fc45a7309057a6f9fe45b04186e6d
문정민 <moondeve41@gmail.com>
2023-04-26 08:55:16 +0900

    EDIT :: CompleteBucket 멤버 확인

--- src/main/kotlin/com/example/munjeongminbackend/domain/bucket/service/CompleteBucketService.kt
+++ src/main/kotlin/com/example/munjeongminbackend/domain/bucket/service/CompleteBucketService.kt
@@ -2,17 +2,30 @@ package com.example.munjeongminbackend.domain.bucket.service
 
 import com.example.munjeongminbackend.domain.bucket.domain.repository.BucketRepository
 import com.example.munjeongminbackend.domain.bucket.exception.BucketNotFoundException
+import com.example.munjeongminbackend.domain.member.domain.repository.MemberRepository
+import com.example.munjeongminbackend.domain.member.exception.MemberNotFoundException
+import com.example.munjeongminbackend.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.util.stream.Collectors
 
 @Service
 class CompleteBucketService (
-        private val bucketRepository: BucketRepository
+        private val bucketRepository: BucketRepository,
+        private val userFacade: UserFacade,
+        private val memberRepository: MemberRepository
 ) {
 
     @Transactional
     fun execute(id: Long) {
         val bucket = bucketRepository.findBucketById(id) ?: throw BucketNotFoundException.EXCEPTION
+        val user = userFacade.getCurrentUser()
+
+        val members = memberRepository.findMembersByBucket(bucket).stream().map { it.user }.collect(Collectors.toList())
+
+        if(!members.contains(user)) {
+            throw MemberNotFoundException.EXCEPTION
+        }
 
         bucket.endBucket(true)
     }
