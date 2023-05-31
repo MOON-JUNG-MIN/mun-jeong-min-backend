0ef5f9c1e9b2a204fa271095389f967267e97f4f
문정민 <moondeve41@gmail.com>
2023-05-31 11:34:07 +0900

    EDIT :: create bucket notblank

--- src/main/kotlin/com/example/munjeongminbackend/domain/bucket/present/dto/BucketCreateRequest.kt
+++ src/main/kotlin/com/example/munjeongminbackend/domain/bucket/present/dto/BucketCreateRequest.kt
@@ -1,10 +1,18 @@
 package com.example.munjeongminbackend.domain.bucket.present.dto
 
 import java.time.LocalDateTime
+import javax.validation.constraints.NotBlank
 
 data class BucketCreateRequest (
+
+        @NotBlank(message = "title 비어있어요")
         val title: String,
+
+        @NotBlank(message = "content 비어있음")
         val content: String,
+
         val image: String?,
+
+        @NotBlank(message = "날짜 요소 비어있음")
         val targetDate: String
 )
