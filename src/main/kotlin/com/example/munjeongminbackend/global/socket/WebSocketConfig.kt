1e78753bc026a03c2fd3c636f46378da4b28078a
문정민 <moondeve41@gmail.com>
2023-06-09 10:23:46 +0900

    EDIT :: config

--- src/main/kotlin/com/example/munjeongminbackend/global/socket/WebSocketConfig.kt
+++ src/main/kotlin/com/example/munjeongminbackend/global/socket/WebSocketConfig.kt
@@ -1,6 +1,8 @@
 package com.example.munjeongminbackend.global.socket
 
+import lombok.extern.slf4j.Slf4j
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
@@ -9,10 +11,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfig (
+        private val headerInterceptor: HeaderInterceptor
 ) : WebSocketMessageBrokerConfigurer {
-
+    
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*")
+
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
@@ -20,4 +24,9 @@ class WebSocketConfig (
         registry.setApplicationDestinationPrefixes("/app")
     }
 
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(headerInterceptor)
+    }
 }
+
+// https://accounts.google.com/o/oauth2/auth?client_id=465541274089-u777afbuec9desm0p4uu4qm1b8um0mcb.apps.googleusercontent.com&redirect_uri=http://localhost:8080/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/userinfo.email
